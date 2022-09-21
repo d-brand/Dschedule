38038b5d474c639f45d0f0ef51745b1364953629
d-brand <kawasaki@d-brand.jp>
2022-09-21 18:14:31 +0900

    Sendgird対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
       address: 'smtp.sendgrid.net',
       port: 587,
       domain: 'heroku.com',
-      user_name: 'apikey',
-      password: ENV['SENDGRID_APIKEY'],
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
       authentication: :plain,
       enable_starttls_auto: true
   }
