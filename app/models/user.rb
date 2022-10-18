732db44b3eeddc909b87604356b72c96bbb5afb0
d-brand <kawasaki@d-brand.jp>
2022-10-18 16:13:06 +0900

    調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable#:confirmable
   devise :database_authenticatable, :registerable, :trackable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:confirmable
   
   validates :email, uniqueness: true
 
