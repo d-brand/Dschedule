43586beb3788f3cf39fb2ca17637c483e98d5215
d-brand <kawasaki@d-brand.jp>
2022-11-17 19:03:51 +0900

    render環境変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rails webpacker:compile
 bundle exec rake db:migrate
-bundle exec run rails db:seed
+bundle exec rails db:seed
